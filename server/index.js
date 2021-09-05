168ca675a1901dc1bd20eda1c5d7d206e324c469
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-09-05 14:45:22 +0530

    full fledged memories website

---
+++ server/index.js
@@ -0,0 +1,26 @@
+
+
+const express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var cors = require("cors");
+const dotenv = require("dotenv");
+
+var postRoutes = require('./routes/posts.js');
+
+var app = express();
+dotenv.config();
+
+
+
+app.use(bodyParser.json({limit:'30mb',extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+app.use('/posts',postRoutes);
+
+//const CONNECTION_URL = 'mongodb+srv://memories:memories@cluster0.4wy3n.mongodb.net/memories?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .catch((error) => console.log(error.message));
---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const {getPosts,createPost, updatePost,deletePost,likePost} = require("../controllers/posts.js");
+
+const router = express.Router();
+
+router.get('/',getPosts);
+router.post('/',createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id',deletePost);
+router.patch('/:id/likePost',likePost);
+
+module.exports = router;
